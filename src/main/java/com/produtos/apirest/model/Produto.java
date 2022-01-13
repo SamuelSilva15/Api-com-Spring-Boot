4afb91694dbbce662a9a30ab8a67694e9d396355
SamuelSilva15 <Silvamsamuel15@gmail.com>
2022-01-13 01:18:32 -0300

    Troquei generate auto por generate identity

--- src/main/java/com/produtos/apirest/model/Produto.java
+++ src/main/java/com/produtos/apirest/model/Produto.java
@@ -18,7 +18,7 @@ public class Produto implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 	
 	@NotNull
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,4 @@
 
-
-insert into TB_PRODUTO (id, nome, quantidade, valor) values (4, 'Iphone 12', 2, 4500);
-insert into TB_PRODUTO (id, nome, quantidade, valor)  values (5, 'Iphone 11', 2, 4000);
-insert into TB_PRODUTO (id, nome, quantidade, valor)  values (6, 'Iphone 10', 2, 3500);
+insert into TB_PRODUTO (nome, quantidade, valor) values ('Iphone 12', 2, 4500);
+insert into TB_PRODUTO (nome, quantidade, valor)  values ('Iphone 11', 2, 4000);
+insert into TB_PRODUTO (nome, quantidade, valor)  values ('Iphone 10', 2, 3500);
