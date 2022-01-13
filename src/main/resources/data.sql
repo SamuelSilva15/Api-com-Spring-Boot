e7d67afb4f5b2831f6f568ee8d0cb32a0d998e06
SamuelSilva15 <Silvamsamuel15@gmail.com>
2022-01-12 21:42:30 -0300

    arrumando ids

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 
 
-insert into TB_PRODUTO (id, nome, quantidade, valor) values (1, 'Iphone 12', 2, 4500);
-insert into TB_PRODUTO (id, nome, quantidade, valor)  values (2, 'Iphone 11', 2, 4000);
-insert into TB_PRODUTO (id, nome, quantidade, valor)  values (3, 'Iphone 10', 2, 3500);
+insert into TB_PRODUTO (id, nome, quantidade, valor) values (4, 'Iphone 12', 2, 4500);
+insert into TB_PRODUTO (id, nome, quantidade, valor)  values (5, 'Iphone 11', 2, 4000);
+insert into TB_PRODUTO (id, nome, quantidade, valor)  values (6, 'Iphone 10', 2, 3500);
