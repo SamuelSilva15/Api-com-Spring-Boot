196f47431d08d6fa6858621130979173840aeffb
SamuelSilva15 <Silvamsamuel15@gmail.com>
2022-01-12 21:34:42 -0300

    Adicionei produtos no data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+
+
+insert into TB_PRODUTO (id, nome, quantidade, valor) values (1, 'Iphone 12', 2, 4500);
+insert into TB_PRODUTO (id, nome, quantidade, valor)  values (2, 'Iphone 11', 2, 4000);
+insert into TB_PRODUTO (id, nome, quantidade, valor)  values (3, 'Iphone 10', 2, 3500);
