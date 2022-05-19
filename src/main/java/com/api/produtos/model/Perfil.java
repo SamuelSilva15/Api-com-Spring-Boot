6039cb02c65c0d57c479e67c342934f3afefe23d
SamuelSilva15 <Silvamsamuel15@gmail.com>
2022-05-19 15:14:23 -0300

    teste de erro

--- src/main/java/com/api/produtos/model/Perfil.java
+++ src/main/java/com/api/produtos/model/Perfil.java
@@ -12,7 +12,7 @@ public class Perfil implements GrantedAuthority {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long id
 
     private String nome;
 
